dee992dc85e2817b336f4975242494c2d67b72f9
Andrew Fitzgerald <apfitzge@gmail.com>
2024-09-06 12:54:05 -0500

    refactor

--- src/transaction_byte_sections.rs
+++ src/transaction_byte_sections.rs
@@ -27,106 +27,136 @@ pub fn get_transaction_byte_sections(
 
     // Split the bytes into sections by content.
     let mut offset = 0;
+    add_signature_sections(transaction, &bytes, sections, &mut offset);
+    add_message_header_sections(transaction, &bytes, sections, &mut offset);
+    add_static_account_keys_sections(transaction, &bytes, sections, &mut offset);
+    add_recent_blockhash_section(transaction, &bytes, sections, &mut offset);
+    add_instructions_sections(transaction, &bytes, sections, &mut offset);
+    add_message_address_table_lookups_sections(transaction, &bytes, sections, &mut offset);
+}
 
-    // Signatures
+fn add_signature_sections(
+    transaction: &VersionedTransaction,
+    bytes: &[u8],
+    sections: &mut Vec<TransactionByteSection>,
+    offset: &mut usize,
+) {
+    let num_signatures = transaction.signatures.len();
+    let num_signature_bytes = 1 + num_signatures * core::mem::size_of::<Signature>();
+    let signature_bytes = bytes[*offset..*offset + num_signature_bytes].to_vec();
+    *offset += num_signature_bytes;
+
+    sections.push(TransactionByteSection {
+        label: "Signatures",
+        bytes: signature_bytes,
+        color: Color::LightGreen,
+    });
+}
+
+fn add_message_header_sections(
+    transaction: &VersionedTransaction,
+    bytes: &[u8],
+    sections: &mut Vec<TransactionByteSection>,
+    offset: &mut usize,
+) {
+    let header_length = 3 + match transaction.version() {
+        TransactionVersion::Legacy(_) => 0,
+        TransactionVersion::Number(_) => 1,
+    };
+    let header_bytes = bytes[*offset..*offset + header_length].to_vec();
+    *offset += header_length;
+
+    sections.push(TransactionByteSection {
+        label: "Message Header",
+        bytes: header_bytes,
+        color: Color::Blue,
+    });
+}
+
+fn add_static_account_keys_sections(
+    transaction: &VersionedTransaction,
+    bytes: &[u8],
+    sections: &mut Vec<TransactionByteSection>,
+    offset: &mut usize,
+) {
+    let num_static_account_keys = transaction.message.static_account_keys().len();
+    let num_static_account_keys_bytes =
+        1 + num_static_account_keys * core::mem::size_of::<Pubkey>();
+    let static_account_keys_bytes =
+        bytes[*offset..*offset + num_static_account_keys_bytes].to_vec();
+    *offset += num_static_account_keys_bytes;
+
+    sections.push(TransactionByteSection {
+        label: "Static Account Keys",
+        bytes: static_account_keys_bytes,
+        color: Color::Yellow,
+    });
+}
+
+fn add_recent_blockhash_section(
+    _transaction: &VersionedTransaction,
+    bytes: &[u8],
+    sections: &mut Vec<TransactionByteSection>,
+    offset: &mut usize,
+) {
+    let num_recent_blockhash_bytes = core::mem::size_of::<Hash>();
+    let recent_blockhash_bytes = bytes[*offset..*offset + num_recent_blockhash_bytes].to_vec();
+    *offset += num_recent_blockhash_bytes;
+
+    sections.push(TransactionByteSection {
+        label: "Recent Blockhash",
+        bytes: recent_blockhash_bytes,
+        color: Color::Magenta,
+    });
+}
+
+fn add_instructions_sections(
+    transaction: &VersionedTransaction,
+    bytes: &[u8],
+    sections: &mut Vec<TransactionByteSection>,
+    offset: &mut usize,
+) {
+    let Ok(num_instruction_bytes) =
+        bincode::serialized_size(&ShortVec(transaction.message.instructions().to_vec()))
+    else {
+        return;
+    };
+    let instruction_bytes = bytes[*offset..*offset + num_instruction_bytes as usize].to_vec();
+    *offset += num_instruction_bytes as usize;
+
+    sections.push(TransactionByteSection {
+        label: "Instructions",
+        bytes: instruction_bytes,
+        color: Color::Cyan,
+    });
+}
+
+fn add_message_address_table_lookups_sections(
+    transaction: &VersionedTransaction,
+    bytes: &[u8],
+    sections: &mut Vec<TransactionByteSection>,
+    offset: &mut usize,
+) {
+    let Some(address_table_lookups) = transaction.message.address_table_lookups() else {
+        return;
+    };
+    let Ok(num_address_table_lookups_bytes) =
+        bincode::serialized_size(&ShortVec(address_table_lookups.to_vec()))
+    else {
+        return;
+    };
+    let address_table_lookups_bytes =
+        bytes[*offset..*offset + num_address_table_lookups_bytes as usize].to_vec();
+
+    // Still want to update offset for consistency
+    #[allow(unused_assignments)]
     {
-        let num_signatures = transaction.signatures.len();
-        let num_signature_bytes = 1 + num_signatures * core::mem::size_of::<Signature>();
-        let signature_bytes = bytes[offset..offset + num_signature_bytes].to_vec();
-        offset += num_signature_bytes;
-
-        sections.push(TransactionByteSection {
-            label: "Signatures",
-            bytes: signature_bytes,
-            color: Color::LightGreen,
-        });
+        *offset += num_address_table_lookups_bytes as usize;
     }
 
-    // Message header
-    {
-        let header_length = 3 + match transaction.version() {
-            TransactionVersion::Legacy(_) => 0,
-            TransactionVersion::Number(_) => 1,
-        };
-        let header_bytes = bytes[offset..offset + header_length].to_vec();
-        offset += header_length;
-
-        sections.push(TransactionByteSection {
-            label: "Message Header",
-            bytes: header_bytes,
-            color: Color::Blue,
-        });
-    }
-
-    // Static Account Keys
-    {
-        let num_static_account_keys = transaction.message.static_account_keys().len();
-        let num_static_account_keys_bytes =
-            1 + num_static_account_keys * core::mem::size_of::<Pubkey>();
-        let static_account_keys_bytes =
-            bytes[offset..offset + num_static_account_keys_bytes].to_vec();
-        offset += num_static_account_keys_bytes;
-
-        sections.push(TransactionByteSection {
-            label: "Static Account Keys",
-            bytes: static_account_keys_bytes,
-            color: Color::Yellow,
-        });
-    }
-
-    // Recent Blockhash
-    {
-        let num_recent_blockhash_bytes = core::mem::size_of::<Hash>();
-        let recent_blockhash_bytes = bytes[offset..offset + num_recent_blockhash_bytes].to_vec();
-        offset += num_recent_blockhash_bytes;
-
-        sections.push(TransactionByteSection {
-            label: "Recent Blockhash",
-            bytes: recent_blockhash_bytes,
-            color: Color::Magenta,
-        });
-    }
-
-    // Instructions
-    {
-        let Ok(num_instruction_bytes) =
-            bincode::serialized_size(&ShortVec(transaction.message.instructions().to_vec()))
-        else {
-            return;
-        };
-        let instruction_bytes = bytes[offset..offset + num_instruction_bytes as usize].to_vec();
-        offset += num_instruction_bytes as usize;
-
-        sections.push(TransactionByteSection {
-            label: "Instructions",
-            bytes: instruction_bytes,
-            color: Color::Cyan,
-        });
-    }
-
-    // Message Address Table Lookups
-    {
-        let Some(address_table_lookups) = transaction.message.address_table_lookups() else {
-            return;
-        };
-        let Ok(num_address_table_lookups_bytes) =
-            bincode::serialized_size(&ShortVec(address_table_lookups.to_vec()))
-        else {
-            return;
-        };
-        let address_table_lookups_bytes =
-            bytes[offset..offset + num_address_table_lookups_bytes as usize].to_vec();
-
-        // Still want to update offset for consistency
-        #[allow(unused_assignments)]
-        {
-            offset += num_address_table_lookups_bytes as usize;
-        }
-
-        sections.push(TransactionByteSection {
-            label: "Message Address Table Lookups",
-            bytes: address_table_lookups_bytes,
-            color: Color::Red,
-        });
-    }
+    sections.push(TransactionByteSection {
+        label: "Message Address Table Lookups",
+        bytes: address_table_lookups_bytes,
+        color: Color::Red,
+    });
 }
