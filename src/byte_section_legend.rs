0e0562319c9bc9fba98557212a53e3d3d31a15bf
Andrew Fitzgerald <apfitzge@gmail.com>
2024-09-09 10:14:13 -0500

    clippy

--- src/byte_section_legend.rs
+++ src/byte_section_legend.rs
@@ -39,7 +39,7 @@ impl<'a> ByteSectionLegend<'a> {
         let mut unique_lines = self
             .sections
             .iter()
-            .filter(|section| section.bytes.len() > 0)
+            .filter(|section| !section.bytes.is_empty())
             .filter(|section| section.label.is_some())
             .map(|section: &TransactionByteSection| &section.label)
             .collect::<HashSet<_>>();
